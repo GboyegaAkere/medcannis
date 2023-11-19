d5816104c11441488dd6421fb1b9b7ce11079418
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-11-19 03:34:51 +0100

    new commit

--- src/data/product.jsx
+++ src/data/product.jsx
@@ -1,6 +1,5 @@
 import Weed1 from "../assets/images/weed1.png"
 import Weed2 from "../assets/images/weed2.png"
-// 
 
 export const products = [
     {  
@@ -16,72 +15,46 @@ export const products = [
         category: "seller",
         flower:"mushroom"
       },
-      {  
-        id:2,
-        image:Weed1,
-        description:"Mix And Match Shatter/Budder",
+      {
+        id: 2,
+        image: Weed2,
+        description: "Indica THC Gummies - 500mg",
+        title: "EDIBLES",
+        reviews: "(78 reviews)",
+        prevPrice: "$35.00",
+        newPrice: "$25.00",
+        energy: "Indica 90%",
+        color: "purple",
+        category: "seller",
+        flower: "cannabis"
+    },
+    {
+        id: 3,
+        image: Weed1,
+        description: "CBD Infused Bath Bombs",
+        title: "TOPICALS",
+        reviews: "(45 reviews)",
+        prevPrice: "$20.00",
+        newPrice: "$15.00",
+        energy: "CBD only",
+        color: "blue",
+        category: "seller",
+        flower: "hemp"
+    },
+    {
+        id: 4,
+        image: Weed2,
+        description: "Hybrid Pre-Rolled Joints Pack",
         title: "FLOWER",
-        reviews: "(123 reviews)",
-        prevPrice: "$140,00",
-        newPrice: "$200.00",
-        energy:"Sativa 100%",
-        color: "white",
+        reviews: "(95 reviews)",
+        prevPrice: "$50.00",
+        newPrice: "$40.00",
+        energy: "Hybrid 50%",
+        color: "green",
         category: "seller",
-        flower:"mushroom"
-      },
-      {  
-        id:3,
-        image:Weed1,
-        description:"Mix And Match Shatter/Budder",
-        title: "WEED",
-        reviews: "(123 reviews)",
-        prevPrice: "$140,00",
-        newPrice: "$200.00",
-        energy:"Sativa 100%",
-        color: "white",
-        category: "seller",
-        flower:"mushroom"
-      },
-
-      // {  
-      //   id:1,
-      //   image:Weed1,
-      //   description:"Mix And Match Shatter/Budder",
-      //   title: "CONCENTRATE",
-      //   reviews: "(123 reviews)",
-      //   prevPrice: "$140,00",
-      //   newPrice: "$200.00",
-      //   energy:"Sativa 100%",
-      //   color: "white",
-      //   category: "seller",
-      //   flower:"mushroom"
-      // },
-      // {  
-      //   id:2,
-      //   image:Weed1,
-      //   description:"Mix And Match Shatter/Budder",
-      //   title: "FLOWER",
-      //   reviews: "(123 reviews)",
-      //   prevPrice: "$140,00",
-      //   newPrice: "$200.00",
-      //   energy:"Sativa 100%",
-      //   color: "white",
-      //   category: "seller",
-      //   flower:"mushroom"
-      // },
-      // {  
-      //   id:3,
-      //   image:Weed1,
-      //   description:"Mix And Match Shatter/Budder",
-      //   title: "WEED",
-      //   reviews: "(123 reviews)",
-      //   prevPrice: "$140,00",
-      //   newPrice: "$200.00",
-      //   energy:"Sativa 100%",
-      //   color: "white",
-      //   category: "seller",
-      //   flower:"mushroom"
-      // },
+        flower: "cannabis"
+    },
+      
   ];
 
   export const products2 = [

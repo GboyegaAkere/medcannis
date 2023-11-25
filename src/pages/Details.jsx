211205087546d6343fdc6f0c5afcd349fb672de6
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2023-11-25 22:44:40 +0100

    new commit

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -13,7 +13,7 @@ const Details = () => {
             <img src={Weed1} alt="" className='w-[400px] h-[400px]' />
           </div>
 
-          <div className='w-[640px] h-[1162px] border bottom-1'>
+          <div className='w-[640px] h-[1000px] border bottom-1'>
             <p>CONCENTRATE</p>
             <div className='w-[640px] h-[76px]'>
              <p className='text-2xl font-bold'>Mix And Match Shatter/Budder 28g (4 X 7G)</p>
@@ -64,10 +64,46 @@ const Details = () => {
                     <p className='text-gray-400 font-semibold'>AROMAS</p>
                     <p className='font-semibold'>Anxiety, Arthritis, Chronic Pain, Depression, Fatigue, Inflammation, Insomnia, Irregular Bowel Movements, Migraines, Mood Swings</p>
                   </div>
+                </div> 
+              </div>
+              <div className='mt-6'>
+                <p>DESCRIPTION</p>
+                <div className='w-[640px] h-[42px]'>
+                  <p className='text-gray-500'>Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.</p>
+                </div>
+              </div>
+
+              <div className='mt-10 flex flex-col gap-4'>
+                <div className='flex flex-row items-center justify-between px-10'>
+                  <p>Khalifa kush(AAA)</p>
+                  <p>$120.00</p>
+                </div>
+                <div className='flex flex-row items-center justify-between px-10'>
+                  <p>Add Integra Pack - 4g</p>
+                  <p>$11.00</p>
+                </div>
+                <div className='flex flex-row items-center justify-between px-10'>
+                  <p>Add Integra Pack - 8g</p>
+                  <p>$1.00</p>
                 </div>
 
-                
+
+                <div className='flex flex-row items-center gap-4 justify-between px-10 mt-10'>
+                    <div className='flex flex-row items-center gap-6'>
+                      <p>-</p>
+                      <div className='w-[36px] h-[36px] bg-gray-200 rounded-lg flex justify-center items-center'>
+                        <p>2</p>
+                      </div>
+                      <p>+</p>
+                    </div>
+                    <div className='w-[250px] h-[56px] rounded-full bg-green-700 flex items-center flex-row justify-center gap-2'>
+                      <p>Add to cart</p>
+                      <p>|</p>
+                      <p>$245.00</p>
+                    </div>
+                  </div>
               </div>
+             
           </div>
       </div>
     </div>

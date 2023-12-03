6dfee5429333907dfd5bfef3c8b63cf81ee63434
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2023-12-03 23:13:17 +0100

    new commit

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div className='w-[390px] h-[1762px] md:w-[1440px] md:h-[871px] mt-6 gap-8 mx-auto flex flex-col md:flex-row justify-center'>
-      <div className=' w-[342px] h-[1199px] md:w-[686px] md:h-[807px] bg-red-300 mx-auto'>
+    <div className='w-[390px] h-[1762px] md:w-[1440px] md:h-[871px] mt-6 gap-10 mx-auto flex flex-col md:flex-row justify-center'>
+      <div className=' w-[342px] h-[1199px] md:w-[686px] md:h-[807px] bg-red-300 mx-auto md:mx-0'>
         <p className='text-2xl'>Your Cart</p>
       </div>
 
-      <div className='w-[342px] h-[1199px] md:w-[420px] md:h-[499px] bg-yellow-300 mx-auto'>
+      <div className='w-[342px] h-[1199px] md:w-[420px] md:h-[499px] bg-yellow-300 mx-auto md:mx-0'>
 
       </div>
     </div>
